7e195f112b75b7ff3fa3111ecbfd9ff4e1a3a0aa
Alkim Gozen <alkim.gozen@matriksdata.com>
2013-06-05 11:37:00 +0300

    added Windows PowerShell script

---
+++ windows/PSConvertToUTF8.ps1
@@ -0,0 +1,8 @@
+﻿$files = Get-ChildItem -recurse "*.xml"
+foreach ( $file in $files )
+{
+    Write-Host $file
+    $MyFile = Get-Content $file
+    $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
+    [System.IO.File]::WriteAllLines($file, $MyFile, $Utf8NoBomEncoding)
+}
